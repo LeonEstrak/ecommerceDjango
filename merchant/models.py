0ff1e933d67185a4217dcae98a33b995dc09eaf1
LeonEstrak <aniket2583@gmail.com>
2021-04-24 15:22:42 +0530

    Made the Index View

--- merchant/models.py
+++ merchant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    entry_dateTime = models.DateTimeField(
+        verbose_name="DateTime of Entry of item into Database", auto_now_add=True
+    )
+    item_name = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+    item_can_be_seen = models.BooleanField(
+        verbose_name="Determines if the item can be seen by the customer"
+    )
+    item_profit_percentage = models.FloatField(
+        verbose_name="Profit margin percentage made on the item"
+    )
+    item_image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.item_name
--- merchant/views.py
+++ merchant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Item
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = "merchant/index.html"
+    context_object_name = "latest_item_list"
+
+    def get_queryset(self):
+        return Item.objects.order_by("entry_dateTime")
